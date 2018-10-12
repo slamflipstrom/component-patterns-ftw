a8323337ebdff51995db242ec712cda4866f484a
Sam Lindstrom <lindstrom.s@gmail.com>
2018-10-12 13:54:03 -0600

    add code-surfer

--- components/PriceTicker/PriceTicker.js
+++ components/PriceTicker/PriceTicker.js
@@ -1,36 +1,38 @@
-import React from "react";
+import React from "react"
 
 // import "./PriceTicker.scss";
 
+const PriceTickerRow = ({ currency, price = "0.00" }) => {
+  return (
+    <div>
+      {currency}: {price}
+    </div>
+  )
+}
+
 class PriceTicker extends React.Component {
   constructor() {
-    super();
+    super()
 
-    this.interval = null;
+    this.interval = null
     this.state = {
-      time: Date.now()
-    };
-  }
-
-  componentDidMount() {
-    // this.props.getPrices(["BTC-USD", "ETH-USD", "SALT-USD", "LTC-USD"]);
-    // this.interval = setInterval(() => {
-    //   this.setState({ time: Date.now() });
-    //   this.props.getPrices(["BTC-USD", "ETH-USD", "SALT-USD", "LTC-USD"]);
-    // }, this.props.updateFrequency ? this.props.updateFrequency : 30000);
+      time: Date.now(),
+    }
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+    clearInterval(this.interval)
   }
 
   render() {
     return (
-      <div className={"PriceTicker"}>
-        <div className="PriceTicker-row">HI GUYS</div>
+      <div className="PriceTicker">
+        <PriceTickerRow currency="BTC" className="PriceTicker-row" />
+        <PriceTickerRow currency="ETH" className="PriceTicker-row" />
+        <PriceTickerRow currency="LTC" className="PriceTicker-row" />
       </div>
-    );
+    )
   }
 }
 
-export default PriceTicker;
+export default PriceTicker
