a860067214778e8bd9eb0db001398825b4f84e6a
lindstrom.s@gmail.com <lindstrom.s@gmail.com>
2018-10-16 16:28:55 -0600

    more updates

--- components/index.js
+++
@@ -1,1 +0,0 @@
-// export { default as PriceTicker } from "./PriceTicker/PriceTicker";
--- theme.js
+++ theme.js
@@ -1,13 +1,16 @@
-import { future, swiss } from "mdx-deck/themes"
+import { future } from "mdx-deck/themes"
 
 import tomorrow from "react-syntax-highlighter/styles/prism/tomorrow"
 
 export default {
   ...future,
   h3: {
-    marginTop: "30px",
+    marginTop: "30px"
+  },
+  li: {
+    fontSize: "34px"
   },
   prism: {
-    style: tomorrow,
-  },
+    style: tomorrow
+  }
 }
